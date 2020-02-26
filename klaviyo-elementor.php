9ac59a46dde0dd6412f0e5f1c02d9c8e978c55f5
achchu93 <achchu.zats@gmail.com>
2020-02-27 02:02:09 +0530

    initial setup

---
+++ klaviyo-elementor.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Klaviyo for Elementor
+ * Plugin URI:        #
+ * Description:       An extension for the Dynamics 365 CRM
+ * Version:           0.1
+ * Requires PHP:      5.6
+ * Author:            Ahamed Arshad
+ * Author URI:        mailto:achchu.zats@gmail.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       klaviyo-elementor
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) || ! defined( 'ELEMENTOR_PRO__FILE__' ) ) {
+	exit; // Exit if accessed directly or Elementor Pro not activated
+}
